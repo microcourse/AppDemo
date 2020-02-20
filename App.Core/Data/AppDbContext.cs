338299ca33513ed392d252efa57cc9314d82bd57
Andrey Dyachenko <dyachenkoav@gmail.com>
2020-02-20 12:22:19 +0200

    Init

---
+++ App.Core/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Core.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ App.Test/AppTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace App.Test
+{
+    public class AppTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
